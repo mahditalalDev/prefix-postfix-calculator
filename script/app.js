4e2271e6a5563e932215e95929f452cddc239fc8
Mahdi Talal <mahditalal.789@gmail.com>
2024-12-20 00:47:16 +0200

    refix decimal number bug+ reformat code and use reusable functions

--- script/app.js
+++ script/app.js
@@ -47,21 +47,8 @@ space_btn.addEventListener("click", () => {
   result.innerHTML = result_text;
 });
 
-//* Check the type of the equation to determine which calculation algorithm should be used
-function equationType(equation) {
-  first_digit = equation[0];
-  last_digit = equation[equation.length - 1];
-  if (isOperator(first_digit)) {
-    prefixCalculation(equation);
-  } else if (isOperator(last_digit)) {
-    postfixCalculation(equation);
-  } else infixCalculation(equation);
-}
-function splitEquation(equation) {
-  let pattern = /[+\-x/]|\d+/g;
-  let tokens = equation.match(pattern);
-  return tokens;
-}
+
+
 function prefixCalculation(equation) {
   let equation_array = splitEquation(equation);
   console.log(equation_array);
@@ -75,44 +62,118 @@ function prefixCalculation(equation) {
           alert("Invalid equation: Not enough operands.");
           return;
         }
-        let first_stack_element = parseFloat(stack.pop());
-        let second_stack_element = parseFloat(stack.pop());
-        switch (equation_array[i]) {
-          case "+":
-            equation_result = first_stack_element + second_stack_element;
-            break;
-          case "-":
-            equation_result = first_stack_element - second_stack_element;
-            break;
-          case "/":
-            if (second_stack_element === 0) {
-              alert("You can't divide by zero.");
-              return "Division by zero error";
-            }
-            equation_result = first_stack_element / second_stack_element;
-            break;
-          case "x":
-            equation_result = first_stack_element * second_stack_element;
-            break;
-          default:
-            throw new Error("Invalid operator");
-        }
+        let first_element = parseFloat(stack.pop());
+        let second_element = parseFloat(stack.pop());
+        let operator = equation_array[i]
+        equation_result = calculate(first_element,second_element,operator)
         stack.push(equation_result);
       } else {
         stack.push(equation_array[i]);
       }
     }
-
-    result_text = equation_result;
-    if (typeof result !== "undefined" && result !== null) {
-      result.innerHTML = result_text;
-    } else {
-      console.log(result_text);
-    }
+    displayResult(equation_result)
     return equation_result;
   } catch (error) {
     alert("Error: " + error.message);
     return;
+  }
+}
+
+/*
+Postfix Evaluation
+Expression: 9 2 3 * +
+[9,2,3,*,+]
+Steps:
+1. Start with the first operator *: 2 * 3 = 6.
+2. Add 9 + 6 = 15.
+Result: 15
+*/
+function postfixCalculation(equation) {
+  let equation_array = splitEquation(equation);
+  let stack = [];
+  let equation_result = 0;
+  let equation_length = equation_array.length;
+  for(let i=0;i<equation_length;i++){
+    if(isOperator(equation_array[i])){
+      if(stack.length<2){
+        throw new Error("not much operant");
+      }
+      else{
+        let first_stack_element = stack.pop()
+        let second_stack_element = stack.pop()
+        calculate(first_stack_element,second_stack_element,equation_array[i])       
+      }
+    }
+    else{
+      stack.push(equation_array[i])
+    }
+
+
+  }
+
+}
+function calculate(first_element , second_element , operator){
+  let number1 = parseFloat(first_element)
+  let number2 = parseFloat(second_element)
+  let  equation_result=0.0;
+  switch (operator) {
+    case "+":
+      equation_result = number1 + number2;
+      break;
+    case "-":
+      equation_result = number1 - number2;
+      break;
+    case "/":
+      if (number2 === 0) {
+        alert("You can't divide by 0");
+        return "Division by zero error";
+      }
+      equation_result = number1 / number2;
+      break;
+    case "x":
+      equation_result = number1 * number2;
+      break;
+    default:
+      throw new Error("Invalid operator");
+  }
+  showAlert(equation_result)
+  return equation_result;
+}
+
+function infixCalculation(equation) {
+  let equation_array = splitEquation(equation);
+  console.log(equation_array)
+  let equation_result = 0;
+  let equation_length = equation_array.length;
+  try {
+    for (let i = 0; i < equation_length - 2; i += 2) {
+      let first_element = equation_array[i];
+      let operator = equation_array[i + 1];
+      let second_element = equation_array[i + 2];
+      if (
+        isNaN(first_element) ||
+        !isOperator(operator) ||
+        isNaN(second_element)
+      ) {
+        alert("Invalid equation format!");
+        throw new Error("Invalid equation");
+      }
+      equation_result = calculate(first_element,second_element,operator)
+    }
+  } catch (error) {
+    showAlert("Invalid equation");
+    console.error(error.message);
+    return "Error: Invalid equation";
+  }
+  displayResult(equation_result)
+  return equation_result;
+}
+function displayResult(equation_result){
+  result_text = equation_result;
+  if (typeof result !== "undefined" && result !== null) {
+    result.innerHTML = result_text;
+  } else {
+    console.log(result_text);
   }
 }
 function isOperator(operant) {
@@ -127,66 +188,18 @@ function isOperator(operant) {
   }
   return false;
 }
-// todo
-function postfixCalculation(equation) {
-  showAlert(`postfix +${equation}`);
+function splitEquation(equation) {
+  let pattern = /[+\-*/]|\d*\.?\d+/g;
+  let tokens = equation.match(pattern);
+  return tokens;
 }
-
-function infixCalculation(equation) {
-  let equation_array = convertToArray(equation);
-  console.log("the array is", equation_array);
-  let equation_result = 0;
-  let equation_length = equation_array.length;
-
-  try {
-    for (let i = 0; i < equation_length - 2; i += 2) {
-      let first_element = parseFloat(equation_array[i]);
-      let operator = equation_array[i + 1];
-      let second_element = parseFloat(equation_array[i + 2]);
-      if (
-        isNaN(first_element) ||
-        !isOperator(operator) ||
-        isNaN(second_element)
-      ) {
-        alert("Invalid equation format!");
-        throw new Error("Invalid equation");
-      }
-      switch (operator) {
-        case "+":
-          equation_result = first_element + second_element;
-          break;
-        case "-":
-          equation_result = first_element - second_element;
-          break;
-        case "/":
-          if (second_element === 0) {
-            alert("You can't divide by 0");
-            return "Division by zero error";
-          }
-          equation_result = first_element / second_element;
-          break;
-        case "x":
-          equation_result = first_element * second_element;
-          break;
-        default:
-          throw new Error("Invalid operator");
-      }
-    }
-  } catch (error) {
-    showAlert("Invalid equation");
-    console.error(error.message);
-    return "Error: Invalid equation";
-  }
-  result_text = equation_result;
-  if (typeof result !== "undefined" && result !== null) {
-    result.innerHTML = result_text;
-  } else {
-    console.log(result_text);
-  }
-  return equation_result;
-}
-
-function convertToArray(equation) {
-  let equation_array = equation.split(/([+\-x*/])/);
-  return equation_array;
+//* Check the type of the equation to determine which calculation algorithm should be used
+function equationType(equation) {
+  first_digit = equation[0];
+  last_digit = equation[equation.length - 1];
+  if (isOperator(first_digit)) {
+    prefixCalculation(equation);
+  } else if (isOperator(last_digit)) {
+    postfixCalculation(equation);
+  } else infixCalculation(equation);
 }
