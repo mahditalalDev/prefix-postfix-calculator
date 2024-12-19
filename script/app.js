31ddb9eac5af402f6f9dcbbb978c38e84966239b
Mahdi Talal <mahditalal.789@gmail.com>
2024-12-19 14:10:12 +0200

    add functions btns (future work) ,styling and add onclick listneer

--- script/app.js
+++ script/app.js
@@ -1,1 +1,10 @@
-console.log("hello Js")
+const buttons = document.querySelectorAll('.button1, .button2');
+function showAlert(msg) {
+    alert(msg);
+}
+
+buttons.forEach(button => {
+    button.onclick = ()=>{
+        showAlert("future work ...")
+    };
+})
