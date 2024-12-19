46fbfdf4bd0d0b492f64354d1354313df839529a
Mahdi Talal <mahditalal.789@gmail.com>
2024-12-19 14:58:05 +0200

    fixied some bugs and enhacne user experince

--- script/app.js
+++ script/app.js
@@ -1,12 +1,16 @@
-const buttons = document.querySelectorAll(".button1, .button2");
-const number_buttons = document.querySelectorAll(".button3");
-const result = document.getElementById('display-area');
-let result_text = '';
+const future_work = document.querySelectorAll(".future_work");
+const number_buttons = document.querySelectorAll(".num_btn");
+const result = document.getElementById("display-area");
+const delete_btn = document.getElementById("del");
+const ac_btn = document.getElementById("ac");
+let result_text = "";
+// !todo add on //of button
+const is_on = false;
 function showAlert(msg) {
   alert(msg);
 }
 
-buttons.forEach((button) => {
+future_work.forEach((button) => {
   button.onclick = () => {
     showAlert("future work ...");
   };
@@ -14,7 +18,13 @@ buttons.forEach((button) => {
 number_buttons.forEach((button) => {
   button.onclick = () => {
     result_text += button.innerHTML;
-    result.innerText=result_text
+    result.innerText = result_text;
   };
 });
-
+ac_btn.addEventListener("click", () => {
+  result_text = "";
+  result.innerHTML = "";
+});
+delete_btn.addEventListener("click", () => {
+    // todo its should delete the last index in result and result_text
+});
