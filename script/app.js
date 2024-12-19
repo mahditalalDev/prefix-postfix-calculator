fdb9f4d3600c6be68f06490eb04cb2dc8d623f1e
Mahdi Talal <mahditalal.789@gmail.com>
2024-12-19 14:28:53 +0200

    finished doing numbers buttons +onclick alert

--- script/app.js
+++ script/app.js
@@ -1,10 +1,17 @@
-const buttons = document.querySelectorAll('.button1, .button2');
+const buttons = document.querySelectorAll(".button1, .button2");
+const number_buttons = document.querySelectorAll(".button3");
 function showAlert(msg) {
-    alert(msg);
+  alert(msg);
 }
 
-buttons.forEach(button => {
-    button.onclick = ()=>{
-        showAlert("future work ...")
-    };
-})
+buttons.forEach((button) => {
+  button.onclick = () => {
+    showAlert("future work ...");
+  };
+});
+number_buttons.forEach((button) => {
+  button.onclick = () => {
+    showAlert(`${button.innerHTML}`);
+  };
+});
+
