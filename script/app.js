a11b51778435d8f7204590557c2a0dbb930d81c2
Mahdi Talal <mahditalal.789@gmail.com>
2024-12-19 16:25:46 +0200

    added equationType function and isOperator function

--- script/app.js
+++ script/app.js
@@ -4,10 +4,12 @@ const result = document.getElementById("display-area");
 let result_text = result.innerHTML;
 const delete_btn = document.getElementById("del");
 const ac_btn = document.getElementById("ac");
-const equal = document.getElementById("equal")
+const equal = document.getElementById("equal");
 const space_btn = document.getElementById("space");
+
 // !todo add on //of button
 const is_on = false;
+
 function showAlert(msg) {
   alert(msg);
 }
@@ -17,24 +19,62 @@ future_work.forEach((button) => {
     showAlert("future work ...");
   };
 });
+
 number_buttons.forEach((button) => {
   button.onclick = () => {
     result_text += button.innerHTML;
     result.innerText = result_text;
   };
 });
+
 ac_btn.addEventListener("click", () => {
   result_text = "";
   result.innerHTML = "";
 });
+
 delete_btn.addEventListener("click", () => {
   result_text = result_text.slice(0, -1);
   result.innerHTML = result_text;
 });
-equal.addEventListener("click",()=>{
-showAlert("equal clicked")
-})
-space_btn.addEventListener("click",()=>{
-    result_text +=" "
-    result.innerHTML = result_text;
-})
+
+equal.addEventListener("click", () => {
+  let equation = result_text;
+  equationType(equation);
+});
+
+space_btn.addEventListener("click", () => {
+  result_text += " ";
+  result.innerHTML = result_text;
+});
+
+function equationType(equation) {
+  first_digit = equation[0];
+  last_digit = equation[equation.length - 1];
+  if (isOperator(first_digit)) {
+    prefixCalculation(equation);
+  } else if (isOperator(last_digit)) {
+    postfixCalculation(equation);
+  } else infixCalculation(equation);
+}
+function prefixCalculation() {
+  showAlert("prefix");
+}
+function postfixCalculation() {
+  showAlert("postfix");
+}
+function infixCalculation() {
+  showAlert("infix");
+}
+
+function isOperator(operant) {
+  if (
+    operant == "+" ||
+    operant == "-" ||
+    operant == "×" ||
+    operant == "+" ||
+    operant == "÷"
+  ) {
+    return true;
+  }
+  return false;
+}
